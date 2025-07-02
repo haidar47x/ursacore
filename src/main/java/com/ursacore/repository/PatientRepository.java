9c48935ea8c020679eb1d7eaa94fb8f42b83926b
Haidar Ali <haidar47x@gmail.com>
2025-07-03 00:14:11 +0500

    Added core repositories

--- src/main/java/com/ursacore/repository/PatientRepository.java
+++ src/main/java/com/ursacore/repository/PatientRepository.java
@@ -1,12 +1,12 @@
 package com.ursacore.repository;
 
-import com.ursacore.entity.Sample;
+import com.ursacore.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface SampleRepository extends JpaRepository<Sample, UUID> {
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
 
 }
