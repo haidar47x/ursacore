39e7bc298432d491cb7c510f165f751c3ca8c478
Haidar Ali <haidar47x@gmail.com>
2025-07-08 00:09:12 +0500

    Refactored entities for MySQL and added a MySQL-specific config

--- src/main/java/com/ursacore/entity/Sample.java
+++ src/main/java/com/ursacore/entity/Sample.java
@@ -7,7 +7,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -23,7 +25,8 @@ public class Sample {
     @Id
     @GeneratedValue(generator = "UUID")
     @UuidGenerator
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
     @Version
