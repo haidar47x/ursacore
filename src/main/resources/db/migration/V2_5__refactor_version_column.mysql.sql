a0a36ffb5a51a4548a526b0234ba3d0cf4d53d63
Haidar Ali <haidar47x@gmail.com>
2025-07-24 17:27:15 +0500

    Refactored version column of test order and order line

--- src/main/resources/db/migration/V2_5__refactor_version_column.mysql.sql
+++ src/main/resources/db/migration/V2_5__refactor_version_column.mysql.sql
@@ -1,3 +1,5 @@
 alter table `test_category` add column version bigint default not null;
 alter table `sample` change column version version bigint default not null;
 alter table `patient` change column version version bigint default not null;
+alter table `test_order` change column version version bigint default not null;
+alter table `test_order_line` change column version version bigint default not null;
