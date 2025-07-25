6bb09653231ecc7d133ba93f1a2ab9926bd402f8
Haidar Ali <haidar47x@gmail.com>
2025-07-25 19:39:02 +0500

    Added TestCategory DTO

--- src/main/java/com/ursacore/model/TestCategoryDTO.java
+++ src/main/java/com/ursacore/model/TestCategoryDTO.java
@@ -1,4 +1,30 @@
 package com.ursacore.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
 public class TestCategoryDTO {
+
+    private UUID id;
+    private Long version;
+
+    @NotNull
+    private TestType testType;
+
+    @NotNull
+    @Size(max = 64)
+    private String categoryName;
+
+    @Size(max = 512)
+    private String description;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
