c487e3c27a46ce0104dc2ff4d56d7cbc10992e2f
Haidar Ali <haidar47x@gmail.com>
2025-07-25 19:39:19 +0500

    Added TestOrderLine DTO

--- src/main/java/com/ursacore/model/TestOrderLineDTO.java
+++ src/main/java/com/ursacore/model/TestOrderLineDTO.java
@@ -1,48 +1,18 @@
-package com.ursacore.entity;
+package com.ursacore.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.annotations.UuidGenerator;
-import org.hibernate.type.SqlTypes;
+import lombok.Builder;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@AllArgsConstructor
+@Data
 @Builder
-@Entity
-@Getter
-@NoArgsConstructor
-@Setter
-public class TestOrderLine {
+public class TestOrderLineDTO {
 
-    @Id
-    @GeneratedValue(generator = "UUID")
-    @UuidGenerator
-    @JdbcTypeCode(SqlTypes.CHAR)
-    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
-
-    @Version
     private Long version;
 
-    @CreationTimestamp
-    @Column(updatable = false)
     private LocalDateTime createdAt;
-
-    @UpdateTimestamp
     private LocalDateTime updatedAt;
-
-    @ManyToOne
-    private TestOrder testOrder;
-
-    @ManyToOne
-    private Sample sample;
-
-    public boolean isNew() {
-        return this.id == null;
-    }
 }
